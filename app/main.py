f4cdcd775e379911552e997f60e8548081daa75d
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-08-30 13:49:48 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 import sys
 
+COMMANDS = {}
 
 def main():
     sys.stdout.write("$ ")
-    
+    command = sys.stdin.readline().strip()
+    if command in COMMANDS:
+        pass
+    else:
+        sys.stdout.write(f'{command}: command not found')
+        
 
 
 if __name__ == "__main__":
