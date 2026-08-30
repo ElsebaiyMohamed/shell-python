2f45e6d48633e53508f8a8cfbcdf9ce50f4a0c75
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-08-30 14:32:31 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,27 @@
 import sys
 
-def _exit():
+def _exit(_):
     sys.exit(0)
     
+def _echo(args: list):
+    for arg in args:
+        sys.stdout.write(arg + " ")
+    sys.stdout.write("\n")
+
 COMMANDS = {
     'exit': _exit,
+    'echo': _echo,
 }
 
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        command = sys.stdin.readline().strip()
+        user_input = sys.stdin.readline().strip()
+        command = user_input.split()[0]
+        args = user_input.split()[1:]
         if command in COMMANDS.keys():
-            COMMANDS.get(command)()
+            COMMANDS.get(command)(args)
         else:
             sys.stdout.write(f'{command}: command not found\n')
         sys.stdout.flush()
