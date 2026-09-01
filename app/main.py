e3514b07182623576ad81cee4c938e1293b9e15c
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-01 03:57:24 +0300

    ADD: feat: run program

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 PATH_LIST  = os.environ.get("PATH", "").split(os.pathsep)
@@ -29,11 +30,21 @@ def _type(args: list):
             return
         is_found, full_path = check_command_in_path(args)
         if is_found:
-            sys.stdout.write(f"{args} is {full_path}")
+            sys.stdout.write(f"{args} is {os.path.splitext(full_path)[0]}")
         else:
             sys.stdout.write(f"{args}: not found")
     sys.stdout.write("\n")
 
+
+def execute_command(command: str, args: list = []):
+    is_found, full_path = check_command_in_path(command)
+    if is_found:
+        subprocess.run([full_path] + args)
+        return True
+    
+    return False
+        
+        
 COMMANDS = {
     'exit': (_exit, 'shell builtin'),
     'echo': (_echo, 'shell builtin'),
@@ -47,17 +58,28 @@ def check_command(command: str):
     else:
         return False
     
+import os
+
 def check_command_in_path(command: str):
+    exec_extensions = ['.exe', '.bat', '.cmd']
     for root_path in PATH_LIST:
-        for dirpath, dirnames, filenames in os.walk(root_path):
-            for filename in filenames: 
-                filename_no_ext, ext = os.path.splitext(filename)
-                if command == filename_no_ext:
-                    full_path = os.path.join(dirpath, filename)
-                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                        return True, os.path.splitext(full_path)[0]
+        if not os.path.isdir(root_path):
+            continue
+        try:
+            for filename in os.listdir(root_path):
+                full_path = os.path.join(root_path, filename)
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    filename_no_ext, ext = os.path.splitext(filename)
+                    if command == filename_no_ext and ext.lower() in exec_extensions:
+                        return True, full_path
+                    if command.lower() == filename.lower():
+                        return True, full_path
+        except PermissionError:
+            continue
                     
     return False, None
+
+
 
     
 def main():
@@ -65,14 +87,17 @@ def main():
         sys.stdout.write("$ ")
         sys.stdout.flush()
         user_input = sys.stdin.readline().strip()
+        if not user_input:
+            continue
         command = user_input.split()[0]
         args = user_input.split()[1:]
         if check_command(command):
             COMMANDS.get(command)[0](args)
-            
         else:
-            NOTFOUND(command)
-            sys.stdout.write("\n")
+            if not execute_command(command, args):
+                NOTFOUND(command)
+            
+        sys.stdout.write("\n")
 
         sys.stdout.flush()
         
